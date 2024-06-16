e39dc4d0d982d7ad62250fa595207eff0d60f5db
Caroline <cjunghei@smail.uni-koeln.de>
2024-06-16 20:13:21 +0200

    feat: implement StudentController with sorted GET Request

--- src/main/java/com/sse/ooseproject/controllers/StudentController.java
+++ src/main/java/com/sse/ooseproject/controllers/StudentController.java
@@ -1,18 +1,37 @@
 package com.sse.ooseproject.controllers;
 
+import com.sse.ooseproject.StudentRepository;
+import com.sse.ooseproject.models.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
-    public StudentController(/* TODO add required repositories. */) {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
     }
 
     @GetMapping("/students")
-    public String students(Model model /* TODO add request parameters. */) {
-        // TODO add functionality.
+    public String students(Model model, @RequestParam String sort_by, @RequestParam Boolean sort_asc) {
+        Sort.Direction sortDirection;
+        if(sort_asc){
+            sortDirection = Sort.Direction.ASC;
+        }else{
+            sortDirection = Sort.Direction.DESC;
+        }
+        List<Student> students = studentRepository.findAll(Sort.by(sortDirection, sort_by));
+        model.addAttribute("sort_by", sort_by);
+        model.addAttribute("sort_asc", sort_asc);
+        model.addAttribute("students", students);
 
         // Returning the name of a view (found in resources/templates) as a string lets this endpoint return that view.
         return "students";
